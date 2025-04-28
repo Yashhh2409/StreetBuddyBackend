1af52b6c6be5974060570ed6d4eb577606ba85a7
Yashhh2409 <dhande.yash2001@gmail.com>
2025-04-28 17:23:15 +0530

    Initial commit

--- config/DB.js
+++ config/DB.js
@@ -1,25 +1,32 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv').config();
 
+// Load environment variables
 const HOST = process.env.HOST;
 const USER = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
 const DATABASE = process.env.DATABASE;
 
-// MySQL connection setup
-const db = mysql.createConnection({
+// Create a connection pool to manage multiple connections
+const pool = mysql.createPool({
   host: HOST,
   user: USER,
   password: PASSWORD,
   database: DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10, 
+  queueLimit: 0
 });
 
-db.connect((err) => {
+// Test the connection
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Database connection failed:', err.stack);
     return;
   }
   console.log('Connected to MySQL');
+  connection.release(); 
 });
 
-module.exports = db;
+
+module.exports = pool;
